8bcfe0610ef7d144c89036b3c00c9e920b837a3a
Nelik <112435537+nelicic@users.noreply.github.com>
2023-03-07 20:41:20 +0200

    repository layer

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using LoggerService;
+using Repository;
 
 namespace CompanyEmployees.Extensions;
 
@@ -28,5 +29,10 @@ public static class ServiceExtensions
     public static void ConfigureLoggerService(this IServiceCollection services)
     {
         services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+
+    public static void ConfigureRepositoryManager(this IServiceCollection services)
+    {
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
     }
 }
---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository;
+
+public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    protected RepositoryContext _repositoryContext;
+
+    public RepositoryBase(RepositoryContext repositoryContext) 
+        => _repositoryContext = repositoryContext;
+
+    public IQueryable<T> FindAll(bool trackChanges)
+        => trackChanges ? _repositoryContext.Set<T>() 
+            : _repositoryContext.Set<T>().AsNoTracking();
+
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        => trackChanges ? _repositoryContext.Set<T>().Where(expression) 
+            : _repositoryContext.Set<T>().Where(expression).AsNoTracking();
+    public void Create(T entity) => _repositoryContext.Set<T>().Add(entity);    
+    public void Update(T entity) => _repositoryContext.Set<T>().Update(entity);
+    public void Delete(T entity) => _repositoryContext.Set<T>().Remove(entity);
+}
