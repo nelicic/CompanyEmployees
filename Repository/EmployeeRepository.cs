4008fe80b3313e2146b637728dbd6a08c84998da
Nelik <112435537+nelicic@users.noreply.github.com>
2023-03-10 17:38:21 +0200

    paging

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Shared.RequestFeatures;
 
 namespace Repository;
 
@@ -21,10 +22,14 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
             .SingleOrDefaultAsync();
     }
 
-    public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges)
+    public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, 
+        EmployeeParameters employeeParameters, bool trackChanges)
     {
-        return await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-            .OrderBy(e => e.Name).ToListAsync();
+        var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            .OrderBy(e => e.Name)
+            .ToListAsync();
+
+        return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
     }
 
     public void DeleteEmployee(Employee employee)
