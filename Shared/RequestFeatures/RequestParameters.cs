bc697328dcfbdb3f2aabae43650d03ce68bebe79
Nelik <112435537+nelicic@users.noreply.github.com>
2023-03-11 01:02:49 +0200

    sorting

--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -11,4 +11,5 @@ public abstract class RequestParameters
         get => _pageSize;
         set => _pageSize = (value > _pageSize) ? maxPageSize : value;
     }
+    public string? OrderBy { get; set; }
 }
