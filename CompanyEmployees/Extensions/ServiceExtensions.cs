94aebf8ec6b60a5b05df5019ab2a61f759f86d8c
Nelik <112435537+nelicic@users.noreply.github.com>
2023-03-11 22:05:43 +0200

    hateoas

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -5,6 +5,8 @@ using Service.Contracts;
 using Service;
 using Microsoft.EntityFrameworkCore;
 using CompanyEmployees.Formatter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CompanyEmployees.Extensions;
 
@@ -57,5 +59,27 @@ public static class ServiceExtensions
     public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder)
     {
         return builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+    }
+
+    public static void AddCustomMediaTypes(this IServiceCollection services)
+    {
+        services.Configure<MvcOptions>(config =>
+        {
+            var systemTextJsonOutputFormatter = config.OutputFormatters
+            .OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
+            if (systemTextJsonOutputFormatter != null)
+            {
+                systemTextJsonOutputFormatter.SupportedMediaTypes
+                .Add("application/vnd.codemaze.hateoas+json");
+            }
+            var xmlOutputFormatter = config.OutputFormatters
+            .OfType<XmlDataContractSerializerOutputFormatter>()?
+            .FirstOrDefault();
+            if (xmlOutputFormatter != null)
+            {
+                xmlOutputFormatter.SupportedMediaTypes
+                .Add("application/vnd.codemaze.hateoas+xml");
+            }
+        });
     }
 }
---
+++ Entities/LinksModels/LinkResourceBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.LinksModels;
+
+public class LinkResourceBase
+{
+    public LinkResourceBase()
+    { }
+
+    public List<Link> Links { get; set; } = new List<Link> { };
+}
