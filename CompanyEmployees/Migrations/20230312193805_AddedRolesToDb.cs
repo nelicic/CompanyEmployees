62cfece1a33b852067fcefd796b25228d2712d7e
Nelik <112435537+nelicic@users.noreply.github.com>
2023-03-13 09:27:17 +0200

    added authentication & authorization with JWT

---
+++ CompanyEmployees/Migrations/20230312193805_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "77c5b32c-c681-49ad-a3c9-e86d277cc832", "1630c5d1-6f77-4e5a-b274-d32834d58a3e", "Manager", "MANAGER" },
+                    { "cdebc7eb-b6d4-44b7-ab66-9196cb8b6d20", "3b9920ec-fdf8-4474-8153-e4580b9c4ffc", "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "77c5b32c-c681-49ad-a3c9-e86d277cc832");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cdebc7eb-b6d4-44b7-ab66-9196cb8b6d20");
+        }
+    }
+}
---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace Presentation.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public AuthenticationController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistrationDto)
+    {
+        var result = await _service.AuthenticationService.RegisterUser(userForRegistrationDto);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto userForAuthenticationDto)
+    {
+        if (!await _service.AuthenticationService.ValidateUser(userForAuthenticationDto))
+            return Unauthorized();
+
+        return Ok(new { Token = await _service.AuthenticationService.CreateToken()});
+    }
+}
